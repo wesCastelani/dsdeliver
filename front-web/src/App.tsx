8d0eda96fa89757d389c8e894468d1be4f3b4fe6
wesCastelani <wesleycastelani96@gmail.com>
2021-01-06 11:03:14 -0300

    Navbar criada

---
+++ front-web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import Navbar from './Navbar/index';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
