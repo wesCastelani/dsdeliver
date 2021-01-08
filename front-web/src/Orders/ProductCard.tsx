ea10292707aadd921a72d674bf6971440e2e7471
wesCastelani <wesleycastelani96@gmail.com>
2021-01-08 14:33:11 -0300

    front web terminado (:

--- front-web/src/Orders/ProductCard.tsx
+++ front-web/src/Orders/ProductCard.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import { formatPrice } from './helpers';
 import { Product } from './types';
 
-type Props = { product: Product };
+type Props = {
+  product: Product;
+  onSelectProduct: (product: Product) => void;
+  isSelected: boolean;
+};
 
-function formatPrice(price: number) {
-  const formater = new Intl.NumberFormat('pt-BR', {
-    style: 'currency',
-    currency: 'BRL',
-    minimumFractionDigits: 2,
-  });
-
-  return formater.format(price);
-}
-
-export default function ProductCard({ product }: Props) {
+export default function ProductCard({
+  product,
+  onSelectProduct,
+  isSelected,
+}: Props) {
   return (
-    <div className="order-card-container">
+    <div
+      className={`order-card-container ${isSelected ? 'selected' : ''}`}
+      onClick={() => onSelectProduct(product)}
+    >
       <h3 className="order-card-title">{product.name}</h3>
       <img
         className="order-card-image"
