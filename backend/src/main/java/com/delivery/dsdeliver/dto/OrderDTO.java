a18b9bed73ad71583fb0ef8ae4838a53bd46bdaf
wesCastelani <wesleycastelani96@gmail.com>
2021-01-11 12:22:30 -0300

    Incluindo total no backend

--- backend/src/main/java/com/delivery/dsdeliver/dto/OrderDTO.java
+++ backend/src/main/java/com/delivery/dsdeliver/dto/OrderDTO.java
@@ -19,18 +19,20 @@ public class OrderDTO implements Serializable {
     private Double longitude;
     private Instant moment;
     private OrderStatus status;
+    private Double total;
 
     private List<ProductDTO> products = new ArrayList<>();
 
     public OrderDTO(){}
 
-    public OrderDTO(Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
+    public OrderDTO(Double total, Long id, String address, Double latitude, Double longitude, Instant moment, OrderStatus status) {
         this.id = id;
         this.address = address;
         this.latitude = latitude;
         this.longitude = longitude;
         this.moment = moment;
         this.status = status;
+        this.total = total;
     }
 
     public OrderDTO(Order entity) {
@@ -41,6 +43,7 @@ public class OrderDTO implements Serializable {
         moment = entity.getMoment();
         status = entity.getStatus();
         products = entity.getProducts().stream().map(x ->new ProductDTO(x)).collect(Collectors.toList());
+        total = entity.getTotal();
     }
 
     public Long getId() {
@@ -98,4 +101,13 @@ public class OrderDTO implements Serializable {
     public void setProducts(List<ProductDTO> products) {
         this.products = products;
     }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
 }
